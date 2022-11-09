eb25e005b7f25a6c1de469d54dd0b78c7559a0d5
SeanNobel <sensho19950503@gmail.com>
2022-11-09 11:03:12 +0000

    update vivit.py

--- vivit.py
+++ vivit.py
@@ -6,16 +6,19 @@ from einops.layers.torch import Rearrange
 from module import Attention, PreNorm, FeedForward
 import numpy as np
 
+
 class Transformer(nn.Module):
-    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout=0.):
         super().__init__()
         self.layers = nn.ModuleList([])
         self.norm = nn.LayerNorm(dim)
         for _ in range(depth):
-            self.layers.append(nn.ModuleList([
-                PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
-                PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
-            ]))
+            self.layers.append(
+                nn.ModuleList([
+                    PreNorm(dim, Attention(dim, heads=heads, dim_head=dim_head, dropout=dropout)),
+                    PreNorm(dim, FeedForward(dim, mlp_dim, dropout=dropout))
+                ]))
 
     def forward(self, x):
         for attn, ff in self.layers:
@@ -24,76 +27,83 @@ class Transformer(nn.Module):
         return self.norm(x)
 
 
-  
 class ViViT(nn.Module):
-    def __init__(self, image_size, patch_size, num_classes, num_frames, dim = 192, depth = 4, heads = 3, pool = 'cls', in_channels = 3, dim_head = 64, dropout = 0.,
-                 emb_dropout = 0., scale_dim = 4, ):
-        super().__init__()
-        
-        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
 
+    def __init__(
+        self,
+        image_size,
+        patch_size,
+        num_frames,
+        num_classes=1,  # FIXME
+        dim=192,
+        depth=4,
+        heads=3,
+        pool='cls',
+        in_channels=3,
+        dim_head=64,
+        dropout=0.,
+        emb_dropout=0.,
+        scale_dim=4,
+    ):
+        super().__init__()
+
+        assert pool in {'cls',
+                        'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
 
         assert image_size % patch_size == 0, 'Image dimensions must be divisible by the patch size.'
-        num_patches = (image_size // patch_size) ** 2
-        patch_dim = in_channels * patch_size ** 2
+        num_patches = (image_size // patch_size)**2
+        patch_dim = in_channels * patch_size**2
         self.to_patch_embedding = nn.Sequential(
-            Rearrange('b t c (h p1) (w p2) -> b t (h w) (p1 p2 c)', p1 = patch_size, p2 = patch_size),
+            Rearrange('b t c (h p1) (w p2) -> b t (h w) (p1 p2 c)', p1=patch_size, p2=patch_size),
             nn.Linear(patch_dim, dim),
         )
 
         self.pos_embedding = nn.Parameter(torch.randn(1, num_frames, num_patches + 1, dim))
         self.space_token = nn.Parameter(torch.randn(1, 1, dim))
-        self.space_transformer = Transformer(dim, depth, heads, dim_head, dim*scale_dim, dropout)
+        self.space_transformer = Transformer(dim, depth, heads, dim_head, dim * scale_dim, dropout)
 
         self.temporal_token = nn.Parameter(torch.randn(1, 1, dim))
-        self.temporal_transformer = Transformer(dim, depth, heads, dim_head, dim*scale_dim, dropout)
+        self.temporal_transformer = Transformer(dim, depth, heads, dim_head, dim * scale_dim,
+                                                dropout)
 
         self.dropout = nn.Dropout(emb_dropout)
         self.pool = pool
 
-        self.mlp_head = nn.Sequential(
-            nn.LayerNorm(dim),
-            nn.Linear(dim, num_classes)
-        )
+        self.mlp_head = nn.Sequential(nn.LayerNorm(dim), nn.Linear(dim, num_classes))
 
     def forward(self, x):
         x = self.to_patch_embedding(x)
         b, t, n, _ = x.shape
 
-        cls_space_tokens = repeat(self.space_token, '() n d -> b t n d', b = b, t=t)
-        x = torch.cat((cls_space_tokens, x), dim=2)
-        x += self.pos_embedding[:, :, :(n + 1)]
+        # cls_space_tokens = repeat(self.space_token, '() n d -> b t n d', b=b, t=t)
+        # x = torch.cat((cls_space_tokens, x), dim=2)
+        x += self.pos_embedding[:, :, :n]  # self.pos_embedding[:, :, :(n + 1)]
         x = self.dropout(x)
 
         x = rearrange(x, 'b t n d -> (b t) n d')
         x = self.space_transformer(x)
         x = rearrange(x[:, 0], '(b t) ... -> b t ...', b=b)
 
-        cls_temporal_tokens = repeat(self.temporal_token, '() n d -> b n d', b=b)
-        x = torch.cat((cls_temporal_tokens, x), dim=1)
+        # cls_temporal_tokens = repeat(self.temporal_token, '() n d -> b n d', b=b)
+        # x = torch.cat((cls_temporal_tokens, x), dim=1)
 
-        x = self.temporal_transformer(x)
-        
+        h = self.temporal_transformer(x)
+        # print(h.shape)
 
-        x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
+        # x = h.mean(dim=1) if self.pool == 'mean' else h[:, 0]
 
-        return self.mlp_head(x)
-    
-    
-    
+        return h.permute(0, 2, 1)
+
 
 if __name__ == "__main__":
-    
+
     img = torch.ones([1, 16, 3, 224, 224]).cuda()
-    
+
     model = ViViT(224, 16, 100, 16).cuda()
     parameters = filter(lambda p: p.requires_grad, model.parameters())
     parameters = sum([np.prod(p.size()) for p in parameters]) / 1_000_000
     print('Trainable Parameters: %.3fM' % parameters)
-    
-    out = model(img)
-    
-    print("Shape of out :", out.shape)      # [B, num_classes]
 
-    
-    
+    out = model(img)
+
+    print("Shape of out :", out.shape)  # [B, num_classes]
